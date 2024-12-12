a04268702bc29f56db52f5bcd6facb8926499713
Elwin van Eede <elwin.vaneede@nordhealth.com>
2024-12-12 15:11:42 +0100

    Add basic example

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>
+import { ref } from "vue";
+
+const name = ref("");
+const count = ref(0);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <provet-stack class="n-reset">
+    <provet-input label="Your name" v-model="name"></provet-input>
+    <p>{{ name }}</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <provet-button variant="primary" @click="count++">
+      Count: {{ count }}
+    </provet-button>
+  </provet-stack>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
